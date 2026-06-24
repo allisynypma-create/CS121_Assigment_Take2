379bba8048ce3395826dcb22be42a96ae74b2dee
Allisyn Ypma <allisynypma@u.boisestate.edu>
2026-06-24 14:32:42 -0600

    Lab Warmup Complete

--- LeapChecker/LeapChecker.java
+++ LeapChecker/LeapChecker.java
@@ -16,7 +16,7 @@ public class LeapChecker
         {
             System.out.println(year + " is not a leap year");
         }
-        else if (year % 4 == 0) 
+        else if (year % 4 == 0)
         {
             System.out.println(year + " is a leap year");
         }
